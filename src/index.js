8314e3100c49211d11980a04d615923dc062a38c
Finn Nguon <36335479+Feenerys@users.noreply.github.com>
2025-07-15 16:58:09 +1000

    feat: basic function to fetch data from api

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import "./styles.css";
 
-console.log("Hello World");
+const URL =
+  "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
+
+var location = "melbourne";
+const key = process.env.WEATHER_API_KEY;
+
+const getData = () => {
+  return fetch(`${URL}${location}?key=${key}`)
+    .then((response) => response.json())
+    .then((data) => {
+      return data;
+    })
+    .catch((error) => console.error(error));
+};
+
+getData().then((data) => console.log(data));
+
+const melb = await getData();
+console.log(melb);
