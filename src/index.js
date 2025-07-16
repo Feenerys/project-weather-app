ce234dbdf632ce56bebe2e82e3e39b296b86ab17
Finn Nguon <36335479+Feenerys@users.noreply.github.com>
2025-07-16 15:14:28 +1000

    feat: add loading indicator after user presses submit

--- src/index.js
+++ src/index.js
@@ -26,20 +26,23 @@ const getData = (location) => {
     });
 };
 
-const countryInput = document.querySelector("#country");
+const locationInput = document.querySelector("#location");
 const form = document.querySelector("form");
 const address = document.querySelector(".address");
 const description = document.querySelector(".description");
 const temp = document.querySelector(".temp");
+const loading = document.querySelector(".loading");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  getData(countryInput.value)
+  loading.textContent = "Loading...";
+  getData(locationInput.value)
     .then((data) => {
       console.log(data);
+      loading.textContent = "";
       address.textContent = data.resolvedAddress;
-      description.textContent = data.description;
       temp.textContent = "Current Temp: " + data.currentConditions.temp;
+      description.textContent = data.description;
     })
     .catch((e) => {
       errorLabel.textContent = e;
